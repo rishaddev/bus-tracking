c463dae4c897d16f7ae4a669e5e49ba7b37e3b31
Rishad Ahamed <rishadcol@gmail.com>
2025-10-03 11:34:57 +0530

    Project setup

---
+++ app/page.js
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <div className="w-screen h-screen bg-white grid justify-center items-center">
+      <div className="text-center">
+        <h1 className="text-4xl font-extrabold text-center text-blue-900 mb-6">
+          Bus Tracking System
+        </h1>
+        <p className="text-2xl font-semibold text-black">Web API Development</p>
+        <p className="text-black font-semibold mb-4">BSc (Hons) in Computing 24.1P</p>
+        <p className="text-black">
+          Coventry ID - 15386607 <br /> Student ID - COBSCCOMP24.1P-009
+          <br /> Student Name - Mohamed Ramzy Rishad Ahamed
+        </p>
+      </div>
+    </div>
+  );
+}
